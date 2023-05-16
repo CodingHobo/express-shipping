0d7d1b4f74877c8a3bf0807e7088bc437ab11548
Tanya Shylock <tourtanya@gmail.com>
2023-05-16 17:41:18 -0600

    add mock for shipProduct

--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -1,11 +1,14 @@
 "use strict";
+const shipItApi = require("../shipItApi");
+
+shipItApi.shipProduct = jest.fn();
 
 const request = require("supertest");
 const app = require("../app");
 
-
 describe("POST /", function () {
   test("valid", async function () {
+    shipItApi.shipProduct.mockReturnValue(123)
     const resp = await request(app).post("/shipments").send({
       productId: 1000,
       name: "Test Tester",
@@ -13,34 +16,29 @@ describe("POST /", function () {
       zip: "12345-6789",
     });
 
-    expect(resp.body).toEqual({ shipped: expect.any(Number) });
+    expect(resp.body).toEqual({ shipped: 123 });
   });
 
   test("throws error if empty request body", async function () {
-    const resp = await request(app)
-      .post("/shipments")
-      .send();
+    const resp = await request(app).post("/shipments").send();
     expect(resp.statusCode).toEqual(400);
   });
 
   test("throws error if incorrect data types in body", async function () {
-    const resp = await request(app)
-      .post("/shipments")
-      .send({
-        "productId": "1001",
-        "name": "ryan",
-        "addr": "123 main st",
-        "zip": "91234"
-      });
+    const resp = await request(app).post("/shipments").send({
+      productId: "1001",
+      name: "ryan",
+      addr: "123 main st",
+      zip: "91234",
+    });
     expect(resp.statusCode).toEqual(400);
     expect(resp.body).toEqual({
-      "error": {
-        "message": [
-          "instance.productId is not of a type(s) integer"
-        ],
-        "status": 400
-      }
+      error: {
+        message: ["instance.productId is not of a type(s) integer"],
+        status: 400,
+      },
     });
   });
 });
+
 
